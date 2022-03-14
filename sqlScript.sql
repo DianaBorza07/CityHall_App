9f45cc02139f777a2af67b5e2320899c0334c01d
diana <dianaborza31@gmail.com>
2022-03-14 15:18:32 +0200

    sql script

---
+++ sqlScript.sql
@@ -0,0 +1,5 @@
+create database labps;
+use labps;
+insert into userrole (id,roleName) values 
+('8945449f-c3f8-4278-a4fd-48ae3e857b97','Administrator'),
+('f44e74c1-9355-4e8a-9877-9bdbbc799a92','Regular user');
